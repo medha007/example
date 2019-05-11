575098bd3ec5b1c86ea928b842d6f520b75c9494
medha007 <nazmunmedha.ruet.15@gmail.com>
2019-05-12 02:35:18 +0600

    medha

--- app/src/main/java/com/example/myapplication/MedhaActivity.java
+++ app/src/main/java/com/example/myapplication/MedhaActivity.java
@@ -8,6 +8,6 @@ public class MedhaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_medha);
+        setContentView(R.layout.activity_medha2);
     }
 }
