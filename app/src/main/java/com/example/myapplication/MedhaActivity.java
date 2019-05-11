71d58f4e1f741e5f5f988eafd2e0de24fb6355b8
medha007 <nazmunmedha.ruet.15@gmail.com>
2019-05-12 02:42:07 +0600

    medha

--- app/src/main/java/com/example/myapplication/MedhaActivity.java
+++ app/src/main/java/com/example/myapplication/MedhaActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MedhaActivity extends AppCompatActivity {
-
+//update project
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
